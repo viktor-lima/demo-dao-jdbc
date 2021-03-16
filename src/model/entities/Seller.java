4a8c84f925cb19f1ac12b54d1428e9bfe6ce68af
viktor-lima <joaolimak10@outlook.com>
2021-03-16 15:29:56 -0300

    Reusing instantiation

--- src/model/entities/Seller.java
+++ src/model/entities/Seller.java
@@ -103,8 +103,8 @@ public class Seller implements Serializable {
 
 	@Override
 	public String toString() {
-		return "Seller [id=" + id + ", name=" + name + ", email=" + email + ", birthDate=" + birthDate + ", baseSalary="
-				+ baseSalary + ", department=" + department + "]";
+		return "Seller [id= " + id + ", name= " + name + ", email= " + email + ", birthDate= " + birthDate + ", baseSalary= "
+				+ baseSalary + ", department= " + department + "]";
 	}
 	
 	
