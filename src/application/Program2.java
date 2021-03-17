9bb9ceb4a8ffe9541b55bef03c7572438d8a2d7f
viktor-lima <joaolimak10@outlook.com>
2021-03-17 20:36:02 -0300

    DepartmentDao findById implementation

---
+++ src/application/Program2.java
@@ -0,0 +1,17 @@
+package application;
+
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
+import model.entities.Department;
+
+public class Program2 {
+	public static void main(String[] args) {
+		
+		DepartmentDao depDao = DaoFactory.createDepartmentDao();
+		System.out.println("========== TEST 1 findById ============");
+		Department dep = depDao.findById(1);
+		System.out.println(dep);
+		
+	}
+
+}
